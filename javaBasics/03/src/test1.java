76e764acf0fa74dc007e6f08a273ee73af335cf5
Why-WU <wuhanyu001@126.com>
2019-02-08 23:21:58 +0800

    java

---
+++ javaBasics/03/src/test1.java
@@ -0,0 +1,12 @@
+package src;
+
+public class test1 {
+
+    public static void main(String[] args) {
+        getMenu();
+    }
+    public static void getMenu() {
+        System.out.print("π¨±£º¶∂°");
+        System.out.print("”„œ„»‚Àø");
+    }
+}
